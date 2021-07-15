1c52148cb772da776e9ed29cf4783488a96083fe
Kelly Jacobs <kelly@kalamuna.com>
2021-07-15 15:03:21 -0700

    CP-52: Fix accidentally added php function in JS

--- web/modules/custom/smart_content_cdn/js/condition_type.cdn.js
+++ web/modules/custom/smart_content_cdn/js/condition_type.cdn.js
@@ -10,10 +10,14 @@
   Drupal.smartContent.plugin.ConditionType = Drupal.smartContent.plugin.ConditionType || {};
 
   Drupal.smartContent.plugin.ConditionType['type:array_select'] = function (condition, value) {
-    let context = value || '';
-
-    if (Array.isArray(context) && context.length > 0) {
-      return in_array(condition.settings['value'] , value);
+    // Make sure that value to check is an array.
+    if (Array.isArray(value) && value.length > 0) {
+      for(var i = 0; i < value.length; i++) {
+        // Check array value with value set in Segment condition.
+        if (value[i] === condition.settings['value']) {
+          return true;
+        }
+      }
     }
 
     return false;
